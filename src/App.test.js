fd32978cb802f5c4ebb193202241481eda961e38
HenryRDavis <rainfallsprings@gmail.com>
2020-09-03 17:39:59 -0500

    ok done now

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent, getByDisplayValue } from "@testing-library/react";
+import { render, fireEvent, getByTestId, act } from "@testing-library/react";
 import App from "./App";
 import ContactForm from "./components/ContactForm";
 
@@ -23,11 +23,14 @@ test("testing contact form", () => {
   const message = getByTestId('message'); 
   const submit = getByTestId('submit'); 
 
-  fireEvent.change(firstName, {target:{value:User.tName}})
-  fireEvent.change(lastName, { target: 'lastName' }) 
-  fireEvent.change(email, {target:'email'})
-  fireEvent.change(message, { target: 'Message' }) 
-  fireEvent.click(submit, { target: 'submit' }) 
-  //remember that you are having to " await" the response of the button. 
+  fireEvent.change(firstName, {target:{value: User.firstName}});
+  fireEvent.change(lastName, {target:{value: User.lastName}});
+  fireEvent.change(email, {target:{value: User.email}});
+  fireEvent.change(message, {target:{value: User.message}});
 
-});
+  act(() => {
+    fireEvent.click(submit);
+    })
+
+  findByText(JSON.stringify(User));
+})
