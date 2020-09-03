d03cee76e7c675eb2802f37c9bf45e07df12384d
HenryRDavis <rainfallsprings@gmail.com>
2020-09-03 16:26:11 -0500

    guess im done

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,31 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, getByText } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test("testing contact form", () => {
+  const { getByPlaceholderText, getByText} = render(<ContactForm />);
+
+  const User = {
+    tName: "Donwg",
+    tLastname: "Dug",
+    tEmail: "mm@email.com",
+    tMessage: "This is my final message, goodbye",
+  };
+
+  const firstName = getByPlaceholderText("Edd");
+  const lastName = getByText('Last Name*');
+  const email =  getByText('Email*');
+  const message = getByText('Message'); 
+
+  fireEvent.change(firstName, {target:{value:User.tName}})
+  fireEvent.change(lastName, { target: 'lastName' }) 
+  fireEvent.change(email, {target:'email'})
+  fireEvent.change(message, { target: 'Message' }) 
+  //remember that you are having to " await" the response of the button. 
+
 });
